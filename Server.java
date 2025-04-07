2b6b3b9c91ec3803101bbbfae7485d5e77fdfa0e
Mintype <96578565+Mintype@users.noreply.github.com>
2025-04-07 17:45:56 -0400

    Simplified Classes

--- Server.java
+++ Server.java
@@ -273,3 +273,17 @@ public abstract class Server {
         return result;
     }
 }
+class MapInteraction {
+    //private ArrayList<String> total;
+
+    public MapInteraction() {
+        //total = new ArrayList<String>();
+    }
+
+    public ArrayList<String> click(String country) {
+        //total.add(country);
+        ArrayList<String> countries = new ArrayList<>();
+        countries.add(country);
+        return countries;
+    }
+}
